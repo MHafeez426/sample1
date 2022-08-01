0e24981d603c57a7b7a84ac3a900c4bd5fc54e0b
MHafeez426 <mohamedhafeez111@gmail.com>
2022-08-01 16:36:40 +0530

    commit

--- operators/bin/selectionstatement.dart
+++ operators/bin/selectionstatement.dart
@@ -1,18 +1,23 @@
+import 'dart:io';
 void main (){
-  int age = 20;
+  int age;
+  print('Enter your age');
+  age = int.parse(stdin.readLineSync()!);
   // simple if
   if(age >= 18){
     print('welcome to vote');
   }else{
     print('not eligible');
   }
-  var month = 'aug';
+  String month;
+  print('Enter the month');
+  month = stdin.readLineSync()!;
   if(month =='jan'){
     print('month is jan');
   }else if (month == 'feb'){
     print('month is feb');
   }else if (month=='april'){
-    print('month is pril');
+    print('month is april');
   }else if (month=='aug') {
     print('month is aug');
   }else if (month == 'sep'){
