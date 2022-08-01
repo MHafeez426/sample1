f8190fdd8ff4bee41928705fa333adabcbb9265a
MHafeez426 <mohamedhafeez111@gmail.com>
2022-08-01 11:20:31 +0530

    commit

---
+++ operators/bin/conditionaloperator.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main (){
+  int age = 20;
+  //syntax: condition ? true statement : false statement ;
+
+  var result = age > 18 ? "welcome to voting centre" : "not eligible";
+  print(result);
+  print('-----------------------------------------');
+
+  String username = 'admin';
+  int password = 123456;
+  var out = (username == 'admin'&& password == 123456) ? 'login successful' : 'login failure';
+  print(out);
+  print('--------------------------------------------');
+  int a = 16;
+  int b = 46;
+  var largest = (a>b) ? '$a' : '$b';
+  print(largest);
+  print('----------------------------------------------');
+int c =78;
+var largest1 = (a>b) ? (a > c ? '$a': '$c') : (b > c ? '$b' : '$c');
+print(largest1);
+print('------------------------------------------------');
+//syntax2 : exp1 ?? exp2 ;
+int? x;
+var result1 = x ?? 'null value' ;
+print(result1);
+}
---
+++ operators/bin/selectionstatement.dart
@@ -0,0 +1,22 @@
+void main (){
+  int age = 20;
+  // simple if
+  if(age >= 18){
+    print('welcome to vote');
+  }else{
+    print('not eligible');
+  }
+  var month = 'aug';
+  if(month =='jan'){
+    print('month is jan');
+  }else if (month == 'feb'){
+    print('month is feb');
+  }else if (month=='april'){
+    print('month is pril');
+  }else if (month=='aug') {
+    print('month is aug');
+  }else if (month == 'sep'){
+    print('month is sept');
+  }
+
+}
