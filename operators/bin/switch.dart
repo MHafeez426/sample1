2e035a8db4b0dd7d5035b17bdd04651e011f1c34
MHafeez426 <mohamedhafeez111@gmail.com>
2022-08-03 08:11:35 +0530

    commit

---
+++ operators/bin/switch.dart
@@ -0,0 +1,43 @@
+
+void main(){
+  var month = 12;
+  switch(month){
+    case 1:
+      print('month is january');
+      break;
+    case 2:
+      print('month is feb');
+      break;
+    case 3:
+      print('month ismarch');
+      break;
+    case 4:
+      print('month is april');
+      break;
+    default :
+      print('none of these');
+
+  }
+  var weekdays = "tuesday";
+  switch(weekdays){
+    case 'monday':
+      print('monday is working ');
+      break;
+    case 'tuesday' :
+      print('tuesday is not working');
+      continue reason;
+    case 'wednesday' :
+      print('wednesday is not working');
+      continue reason;
+    reason:
+  case'holiday':
+    print('public holiday');
+    break;
+    case 'thursday':
+      print('thursday is working day');
+
+
+  }
+}
+
+  
